7bc686dbe4aa18bbe4ed03229b51dc12a8b1f4b3
Yuval Itah <yuval18998@gmail.com>
2021-01-10 16:46:44 +0200

    delete unused type declaration

--- src/Types/ReduxStates/ReduxStates.ts
+++
@@ -1,5 +0,0 @@
-import { IOrderState } from "../../Interfaces/IOrder/IOrderState/IOrderState";
-import { IAuthState } from "../../Interfaces/IAuth/IAuthState/IAuthState";
-import { IBurgerBuilderState } from "../../Interfaces/IBurgerBuilder/IBurgerBuilderState/IBurgerBuilderState";
-
-export type ReduxState = IOrderState | IAuthState | IBurgerBuilderState;
